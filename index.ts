849107f4d0351aab29939982fa1282214a21a97c
Alistair Colling <a@colli.ng>
2023-07-20 12:09:10 +0100

    feat: added simple array fns

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 export const myFun = (printMe: string): string => {
 	console.log('printMe', printMe)
+	const manyStrings: string[] = Array.from(printMe, (current, i) => `I am number ${i}: ${current}`)
+	console.log(`manyStrings: ${JSON.stringify(manyStrings, null, 2)}`)
 	return `I am the passed string!..... ${printMe}`
 }
-
 
 const foo = myFun('good morning')
 
